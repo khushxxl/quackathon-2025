39fb3da795a613650dbf822b41cbcba7fcb3f0d9
Khushaal Choithramani <khushaal.choithramani@gmail.com>
2025-03-09 01:14:20 +0000

    chatbot

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,12 @@
 "use client";
 import Link from "next/link";
-
+import { useRouter } from "next/navigation";
 interface HeaderProps {
   scrollToSection: (sectionId: string) => void;
 }
 
 export default function Header({ scrollToSection }: HeaderProps) {
+  const router = useRouter();
   return (
     <header className="top-0 sticky z-50 bg-white dark:bg-gray-900 shadow-sm dark:shadow-gray-800/20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -48,6 +49,13 @@ export default function Header({ scrollToSection }: HeaderProps) {
               className="font-poppins text-gray-700 dark:text-gray-300 hover:text-green-600 dark:hover:text-green-400 px-3 py-2 text-sm"
             >
               Feedback
+            </button>
+
+            <button
+              onClick={() => router.push("/forum")}
+              className="font-poppins  dark:text-gray-300 bg-gradient-to-r from-green-500 to-green-600 text-white dark:hover:text-white px-3 py-2 text-sm rounded transition-colors"
+            >
+              Community Forums
             </button>
           </nav>
         </div>
